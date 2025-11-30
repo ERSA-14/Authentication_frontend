9c843c223c049c42c437cfcbb7d5b888511b8dbe
saksham <saksham14sg@gmail.com>
2025-11-30 22:34:38 +0530

    final Implement user authentication and database integration

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ import session from "express-session";
 import passport from "passport";
 
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import env from "dotenv";
 
 const saltRounds = 14;
 
 const app = express();
 const port = 3000;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -30,12 +33,14 @@ const db = new pg.Client({
 
 db.connect()
 
-app.use(session({
-  secret:"ersa",
-  resave:false,
-  saveUninitialized:true,
-  cookie:{maxAge:1000*60*60*6},
-},));
+app.use(
+  session({
+    secret: process.env.SESSION_COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 6 },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -69,6 +74,26 @@ app.get("/secrets", (req, res) => {
   }
 });
 
+app.get("/auth/google",passport.authenticate("google",{
+  scope:["profile","email"],
+
+}));
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout",(req,res)=>{
+  req.logout((err)=>{
+  if (err) console.log(err);
+  res.redirect("/");
+});
+})
+
 app.post(
   "/login",
   passport.authenticate("local", {
@@ -82,7 +107,7 @@ app.post("/register", async (req, res) => {
   const password = req.body.password;
   
   if (req.body.username.length == 0 || req.body.password.length==0) {
-      res.send("<h1>Email / password not valid</h1>");
+      res.send("<h1> Email / password not valid </h1>");
   } else {
   
     try {
@@ -98,7 +123,7 @@ app.post("/register", async (req, res) => {
   
               const result = await db.query("INSERT INTO users(email,password) VALUES ($1,$2) RETURNING *",[
                 req.body.username,
-                encrypt(hash,"ersa")]
+                encrypt(hash,process.env.HASHING_SECRET)]
               );
               const user = result.rows[0];
               req.login(user,(err)=>{
@@ -115,12 +140,12 @@ app.post("/register", async (req, res) => {
   }
 });
 
-passport.use(new Strategy( async function verify(username,password,cb){
+passport.use("local",new Strategy( async function verify(username,password,cb){
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [username,]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = decrypt(user.password,"ersa");
+      const storedPassword = decrypt(user.password,process.env.HASHING_SECRET);
 
       bcrypt.compare(password,storedPassword,(err,result)=>{
         if (err){
@@ -146,6 +171,32 @@ passport.use(new Strategy( async function verify(username,password,cb){
 )
 );
 
+passport.use(
+  "google",
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    
+  },async (accessToken,refreshToken,profile,cb)=>{
+    console.log(profile);
+    try{
+      const result = await db.query("SELECT * FROM users WHERE email = $1",[profile.email]);
+      if (result.rows.length === 0){
+        const NewUser = await db.query("INSERT INTO users (email,password) VALUES ($1,$2);",[profile.email,"google"]);
+        cb(null,NewUser.rows[0]);
+      } else {
+        cb(null,result.rows[0]);
+      }
+
+    } catch (err){
+      cb(err);
+
+    }
+  })
+);
+
 passport.serializeUser((user,cb)=>{
   cb(null,user);
 });
@@ -153,7 +204,6 @@ passport.serializeUser((user,cb)=>{
 passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
